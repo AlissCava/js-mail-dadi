8e0a1852468aee4668ceb40853c72345916aa295
AlissCava <cavalleraalissa@gmail.com>
2023-11-22 17:02:49 +0100

    commenti in italiano

---
+++ Mail.js
@@ -0,0 +1,11 @@
+// Creiamo un array con le email autorizzate
+
+
+
+// Chiediamo all'utente di inserire la sua email
+
+// Verifichiamo se l'email dell'utente è presente nella lista
+
+     // Se è presente, stampiamo un messaggio di accesso consentito
+
+     // Se non è presente, stampiamo un messaggio di accesso negato
